a8e9e0510eb1f01c84bae5ba17d2cbd9a8cfe893
Eric Riddoch <eric.russia97@gmail.com>
2023-06-03 03:33:42 -0600

    post video: adding test job against wheel

---
+++ tests/my_folder/test_my_nested_file.py
@@ -0,0 +1,4 @@
+from packaging_demo.my_folder.my_nested_file import CONSTANT
+
+def test__constant_val():
+    ...
--- tests/test_slow.py
+++ tests/test_slow.py
@@ -2,5 +2,7 @@ import pytest
 from packaging_demo.slow import slow_add
 
 
+@pytest.mark.slow
 def test__slow_add():
-    slow_add
+    sum_ = slow_add(1, 2)
+    assert sum_ == 3
